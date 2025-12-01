e369a5cc70513d6916c3c295e393ac1d765664e1
clocky <clockyTestEmail@gmail.com>
2025-12-01 18:20:55 +0300

    solve: two sum (arrays & hashing)

---
+++ Arrays and Hashing/two_sum/two_sum.py
@@ -0,0 +1,17 @@
+class Solution:
+  def twoSum(self, nums: list[int], target: int) -> list[int]:
+    numbers_map = {}
+
+    for i, num in enumerate(nums):
+
+      if target - num in numbers_map.keys():
+
+        return [numbers_map[target - num], i]
+      numbers_map[num] = i
+
+    return None
+
+if __name__ == '__main__':
+  nums = [3, 4, 5, 6]
+  target = 7
+  print(Solution().twoSum(nums, target))
